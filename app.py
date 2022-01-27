e33ee51073505be5329dacdd6d72a70239f1b196
John Sattari <johnsattari@johns-air.lan>
2022-01-26 23:04:11 -0800

    Attempted to fix heroku 500 internal error by adding create table functions

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Lists {self.id}>'
 
+# solution for heroku 500 Internal Error: https://stackoverflow.com/a/69814221
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 # main list
 @app.route('/', methods=['POST', 'GET'])
 def index():
