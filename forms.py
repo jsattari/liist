a78ce388311fa5c8ade69ebb2cca85b3c158ab62
John Sattari <johnsattari@johns-air.lan>
2023-01-02 22:38:52 -0800

    added notes to forms.py

--- forms.py
+++ forms.py
@@ -10,12 +10,38 @@ from models import User
 
 
 class LoginForm(FlaskForm):
+    """
+    Class that represents login forms
+
+    Attributes
+    ----------
+    email : str
+        email address used by user for registration
+    password : str
+        password applied at registration for registered email address
+    """
+
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
     password = PasswordField(
         validators=[InputRequired(), Length(min=8, max=16)])
 
 
 class NewUserForm(FlaskForm):
+    """
+    New user login form
+
+    Attributes
+    ----------
+    email : str
+        email address that will be used for subseqent logins
+    username : str
+        alias that will be used for display on internal pages
+    password : str
+        8-16 character phrase that will be used for future logins
+    check_password : str
+        verification field. must be exact match for password field
+    """
+
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
     username = StringField(validators=[InputRequired(), Length(1, 32)])
     password = PasswordField(validators=[InputRequired(), Length(8, 16)])
